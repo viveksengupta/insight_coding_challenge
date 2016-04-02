64b00ec2c6c2629823305accfbec82b9cf12e726
Vivek Sengupta <sviveksengupta@gmail.com>
2016-04-02 14:15:10 -0700

    added comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # insight_coding_challenge
 coding challenge for Insight data engineering fellowship - process Twitter data and calculate average degree for hashtag graph
+
+Dependencies:
+org.json library
--- src/AverageDegree.java
+++ src/AverageDegree.java
@@ -5,24 +5,31 @@ import java.io.*;
 
 public class AverageDegree {
     public static void main(String[] args) {
+        // try obtaining a FileReader for tweets.txt, catch FileNotFoundException @catch(fnfe)
         try {
             Reader reader = new FileReader("tweet_input/tweets.txt");
 
+            // try-with-resources(BufferedReader), catch IOExeption @catch(ioe)
             try (BufferedReader bufferedReader =
                          new BufferedReader(reader)) {
 
                 String line = bufferedReader.readLine();
                 while (line != null) {
-                    System.out.println(line);
+                    // line has data for one tweet as a JSON object
 
+
+                    // read next line and continue in the while loop
                     line = bufferedReader.readLine();
                 }
 
-            } catch (Exception e) {
-                e.printStackTrace();
+            // @catch(ioe)
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        // @catch(fnfe)
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
         }
     }
 }
