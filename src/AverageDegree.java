69621f7a5b44965daa12a1a9112b24087b1a9cf7
Vivek Sengupta <sviveksengupta@gmail.com>
2016-04-02 22:07:11 -0700

    added data structures, 1 BinaryHeap and 3 HashMaps with ample comments

--- src/AverageDegree.java
+++ src/AverageDegree.java
@@ -2,6 +2,7 @@
  * Created by Vivek on 4/2/16.
  */
 
+import org.apache.commons.collections.BinaryHeap;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,7 +10,9 @@ import org.json.JSONObject;
 import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 public class AverageDegree {
     public static void main(String[] args) {
@@ -21,15 +24,55 @@ public class AverageDegree {
         // Does the current tweet have a timestamp within 60 seconds of max or greater than max
         boolean currTweetInScope = false;
 
+        /* ========== data structures ========== */
+
+        /* ---------- 1. BinaryHeap ----------
+        /* min heap of tweet timestamps
+        /* only insertions and deletions/extractions are needed
+        /* using BinaryHeap implementation from org.apache.commons.collections
+         */
+        BinaryHeap dateHeap = new BinaryHeap();
+
+        /* ---------- 2. HashMap<Date, ArrayList<String>> ----------
+        /* Hashtable with tweet dates as keys and list of edge Strings formed by the hashtags as values
+        /* example tweet: Date=>date123, Hashtags=> #a, #b, #c
+        /* HashMap entry: < date123: < "a-b", "a-c", "b-c" > >
+        /* using standard Java HashMap implementation
+         */
+        HashMap<Date, ArrayList<String>> dateEdgeMap = new HashMap<>();
+
+        /* ---------- 3. HashMap<String, Integer> ----------
+        /* Hashtable with edge Strings as keys and how many tweets contributed those edges as values
+        /* example tweets: tweet1=> #a, #b, #c;   tweet2=> #a, #c
+        /* respective edges: tweet1=> a-b, a-c, b-c;   tweet2=> a-c
+        /* HashMap entries: <"a-b": 1>, <"a-c": 2>, <"b-c": 1>
+        /* "a-c" has value 2 because 2 tweets contributed to that edge
+        /* we maintain contributions only from tweets in the 60 second window
+        /* using standard Java HashMap implementation
+         */
+        HashMap<String, Integer> edgeContributionMap = new HashMap<>();
+
+        /* ---------- 4. HashMap<String, Integer> ----------
+        /* Hashtable with vertices(hashtags) as keys and degrees as values
+        /* example tweets: tweet1=> #a, #b, #c;   tweet2=> #a, #d
+        /* HashMap entries: <"a": 3>, <"b": 2>, <"c": 2>, <"d": 1>
+        /* "a" has 3 edges to "b", "c" and "d";   "d" has only 1 edge to "a"
+        /* this HashMap lets us determine when a vertex is not connected to anyone else
+         */
+        HashMap<String, Integer> vertexDegreeMap = new HashMap<>();
+
+
+
         // try obtaining a FileReader for tweets.txt, catch FileNotFoundException @catch(fnfe)
         try {
             //Reader reader = new FileReader("tweet_input/tweets.txt");
             // delete this line later
             Reader reader = new FileReader("tweet_input/abc.txt");
+            Writer writer = new FileWriter("tweet_output/output.txt");
 
             // try-with-resources(BufferedReader), catch IOExeption @catch(ioe)
-            try (BufferedReader bufferedReader =
-                         new BufferedReader(reader)) {
+            try (BufferedReader bufferedReader = new BufferedReader(reader);
+                 BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
 
                 String line;
 
@@ -97,6 +140,8 @@ public class AverageDegree {
         // @catch(fnfe)
         } catch (FileNotFoundException fnfe) {
             fnfe.printStackTrace();
+        }  catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 
